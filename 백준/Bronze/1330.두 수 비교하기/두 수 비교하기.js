fe08ddeb66753eebf18d1759b7982de85120e5fc
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2022-02-21 23:07:34 +0900

    [Bronze IV] Title: 두 수 비교하기, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/1330.두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,14 @@
+let fs = require('fs'); 
+let input = fs.readFileSync('/dev/stdin').toString().split(' '); 
+let a = parseInt(input[0]); 
+let b = parseInt(input[1]);
+
+if ( a < b) 
+    console.log( "<" )
+
+if ( a > b) 
+    console.log( ">" )
+
+if ( a === b) 
+    console.log( "==" )
+
